71135b192698f2f331aff1beca7f65d32235101c
ancaza12 <56890947+ancaza12@users.noreply.github.com>
2019-12-04 12:38:14 -0500

    1 punto

--- 1.py
+++ 1.py
@@ -40,3 +40,28 @@ for i in range(len(datos)):
     print(datos[:i+1])
     plot_likelihood(datos[:i+1])
 plt.savefig("sigma.png")
+##Tomado de ejercicio del laboratorio de Fourier
+#https://github.com/ComputoCienciasUniandes/MetodosComputacionalesDatos/blob/master/hands_on/solar/monthrg.dat
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+print("2 Ejercicio")
+data = np.loadtxt('monthrg.dat')
+
+def DFT(x):
+    """
+    Compute the discrete Fourier Transform of the 1D array x
+    :param x: (array)
+    """
+    N = x.size
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    e = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(e, x)
+
+plt.figure()
+plt.scatter(data[3481:,0],data[3481:,4])
+plt.plot(data[3481:,1],data[3481:,4])
+plt.savefig("solar.png")
